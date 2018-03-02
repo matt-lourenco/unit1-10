80c9d690869efe9546a9ccb9504d38dc375dbe2e
Matthew Lourenco <matthew.lourenco@ocsbstudent.ca>
2018-03-02 17:47:34 +0000

    Finished enumerated planets and locations

--- Planet.java
+++ Planet.java
@@ -6,101 +6,31 @@
  * 
  ****************************************************************************/
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Planet {
+public enum Planet {
 	
-	public PlanetName name;
-	public int location = 0;
+	MERCURY (57),
+	VENUS (108),
+	EARTH (150),
+	MARS (228),
+	JUPITER (779),
+	SATURN (1430),
+	URANUS (2880),
+	NEPTUNE (4500);
 	
-	public Planet(PlanetName title) {
-		this.name = title;
-		//initialize location (In millions of kilometres)
-		location = Planet.findLocation(name);
-	}
+	private final double location; //In millions of kilometers
 	
-	public enum PlanetName {
-		MERCURY,
-		VENUS,
-		EARTH,
-		MARS,
-		JUPITER,
-		SATURN,
-		URANUS,
-		NEPTUNE,
-	}
-	
-	public static int findLocation(PlanetName nameOfPlanet) {
-		//Use a switch to assign locations
-		switch(nameOfPlanet) {
-		case MERCURY: return 57;
-		case VENUS: return 108;
-		case EARTH: return 150;
-		case MARS: return 228;
-		case JUPITER: return 779;
-		case SATURN: return 1430;
-		case URANUS: return 2880;
-		case NEPTUNE: return 4500;
-		default: return 0;
-		}
+	private Planet(int location) {
+		//default constructor
+		this.location = location;
 	}
 	
 	public static void main(String[] args) {
 		//Test program by calling different planets
 		
-		String input = "";
-		PlanetName planetInput = PlanetName.EARTH;
+		for(Planet planet: Planet.values()) {
+			System.out.println(planet+" is an average of "+planet.location+
+					" million kilometers away from the sun.");
+		}
 		
-		BufferedReader reader = new BufferedReader(new InputStreamReader
-				(System.in));
-	validInput:
-		while(true) {
-			System.out.println("Enter the name of a planet: ");
-			try {
-				//Get user input
-				input = reader.readLine();
-			} catch (IOException noInput) {
-				noInput.printStackTrace();
-			}
-			//test if input is in enumerated list
-			for(PlanetName planet: PlanetName.values()){
-				if(input.toUpperCase().equals(planet.toString())) {
-					break validInput;
-				}
-			}
-		}
-		switch(input.toUpperCase()) {
-		case "MERCURY":
-			planetInput = PlanetName.MERCURY;
-			break;
-		case "VENUS":
-			planetInput = PlanetName.VENUS;
-			break;
-		case "EARTH":
-			planetInput = PlanetName.EARTH;
-			break;
-		case "MARS":
-			planetInput = PlanetName.MARS;
-			break;
-		case "JUPITER":
-			planetInput = PlanetName.JUPITER;
-			break;
-		case "SATURN":
-			planetInput = PlanetName.SATURN;
-			break;
-		case "URANUS":
-			planetInput = PlanetName.URANUS;
-			break;
-		case "NEPTUNE":
-			planetInput = PlanetName.NEPTUNE;
-			break;
-			default: break;
-		}
-		Planet chosenPlanet = new Planet(planetInput);
-		System.out.println("The planet you chose was " + chosenPlanet.name);
-		System.out.println(chosenPlanet.name + " is an average of " + 
-				chosenPlanet.location+" million kilometers away from the sun");
 	}
 }
